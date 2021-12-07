516225c18b02fe27b1915507cc606f4e281fac50
Marina Mosti <marina@mosti.com.mx>
2021-12-07 16:24:39 +0100

    chore: add example

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyCounter ref="counter" />
+
+  <button @click="reset">Reset from parent</button>
+  <button @click="terminate">Terminate from parent</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyCounter from '@/components/MyCounter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyCounter
+  },
+  methods: {
+    reset () {
+      this.$refs.counter.reset()
+    },
+    terminate () {
+      this.$refs.counter.terminate()
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,29 @@
+<script>
+import { ref, h } from 'vue'
+
+export default {
+  setup () {
+    const counter = ref(0)
+
+    const interval = setInterval(() => {
+      counter.value++
+    }, 1000)
+
+    const reset = () => {
+      counter.value = 0
+    }
+
+    const terminate = () => {
+      clearInterval(interval)
+    }
+
+    // context.expose({ reset })
+
+    return () => h('div', [
+      h('p', `Counter: ${counter.value}`),
+      h('button', { onClick: reset }, 'Reset'),
+      h('button', { onClick: terminate }, 'Terminate')
+    ])
+  }
+}
+</script>
